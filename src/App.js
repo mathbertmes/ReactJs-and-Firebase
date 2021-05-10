a95bfb5543d430e59e3f03d33e6080573e9b1b7c
Matheus Bertemes <mathbertemes@gmail.com>
2021-05-10 09:44:53 -0300

    primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+
+function Cabeca(){
+  return(
+    <div className="cabeca">
+      <p>Teste</p>
+    </div>
+  )
+}
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <body>
+      <div>
+        <div className="cabeca">
+          <p className="branco">HMAX</p>
+        </div>
+
+        <div>
+
+          <div id="area-formulario">
+            <form>
+              <label className="branco">Qual o seu destino?</label><br/>
+              <input id="destino"></input><br/><br/>
+              <div id="check">
+                <div>
+                  <label className="branco">Check-In:</label><br/>
+                  <input className="checka"></input>
+
+                </div>
+                <div>
+                  <label className="branco">Check-Out:</label><br/>
+                  <input className="checka"></input>
+
+                </div>
+
+              </div><br/>
+              <div id="qtd">
+                <div>
+                  <label className="branco">Nº de adultos:</label><br/>
+                  <input type="number" className="qtda"></input>
+                </div>
+                <div>
+                  <label className="branco">Nº de Crianças:</label><br/>
+                  <input type="number" className="qtda"></input>
+                </div>
+                <div>
+                  <label className="branco">Nº de Quartos:</label><br/>
+                  <input type="number" className="qtda"></input>
+                </div>
+
+              </div>
+
+            </form>
+
+          </div>
+
+        </div>
+          
+      </div>
+    </body>
   );
 }
 
 export default App;
+
